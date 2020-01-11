2e70c68f9f44d2a2da6cb91a88ece80f0247326e
Arturo Silva <arturo.silva@ciencias.unam.mx>
2020-01-11 11:02:22 -0600

    feat: Dockerfile, K8s manifest and extra points

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+import os
+from flask import Flask,request
+from flask_redis import FlaskRedis
 
 app = Flask(__name__)
+app.config['REDIS_URL'] = os.getenv('REDIS_URL') or "redis://localhost:6379/0"
+redis_client = FlaskRedis(app)
 
-@app.route("/")
+
+@app.route("/", methods = ["GET", "POST"])
 def hello():
-    return "Hello World!!!"
-
+    global redis_client
+    if request.method == "GET":
+        redis_client.incr('counter',0)
+        return ('Hello World!!! # request POSTS: {}'.format(redis_client.get('counter').decode('UTF-8')))
+    if request.method == "POST":
+        redis_client.incr("counter")
+        return "OK! request POSTS+1"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
