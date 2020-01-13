84fd83f06c227e13570580a3238b724e55974262
Arturo Silva <arturo.silva@ciencias.unam.mx>
2020-01-13 14:20:32 -0600

    feat: set flag debug=False

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def hello():
         return "OK! request POSTS+1"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
